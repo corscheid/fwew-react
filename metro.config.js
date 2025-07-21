f08d179c3d31bf0f40a711ffda8510c6c4f26dd6
Corey Scheideman <corscheid@gmail.com>
2025-07-21 13:37:05 -0500

    Update eslint and package.json and fix the metro bundler issue

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,23 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
+const ALIASES = {
+  'tslib': require.resolve('tslib/tslib.es6.js'),
+};
+
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  return context.resolveRequest(
+    context,
+    ALIASES[moduleName] ?? moduleName,
+    platform
+  );
+};
 
 module.exports = config;
