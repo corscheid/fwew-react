31c1f9fe5064561d84559882dff3be4cf59972d7
Corey Scheideman <corscheid@gmail.com>
2025-11-10 16:50:04 -0600

    Remove unused lint rule disabling comment

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,6 @@ const ALIASES = {
 };
 
 /** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
