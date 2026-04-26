1a007621d3d2b973fe970da0a8269f2f02a4887e
Corey Scheideman <corscheid@gmail.com>
2026-04-26 02:32:23 -0500

    Update Audio URL based on selected Dialect

--- constants/AudioResources.ts
+++ constants/AudioResources.ts
@@ -1,3 +1,10 @@
-export default {
-  URL: "https://s.learnnavi.org/audio/vocab",
+import type { Dialect } from "@/types/common";
+
+const AudioResources: { URL: Record<Dialect, string> } = {
+  URL: {
+    forest: "https://s.learnnavi.org/audio/vocab",
+    reef: "https://s.learnnavi.org/audio/vocab/reef",
+  },
 };
+
+export default AudioResources;
--- hooks/useSound.ts
+++ hooks/useSound.ts
@@ -1,9 +1,25 @@
 import AudioResources from "@/constants/AudioResources";
+import { useDialectContext } from "@/context/DialectContext";
+import { Dialect } from "@/types/common";
 import { useAudioPlayer } from "expo-audio";
 import { useState } from "react";
 
+function getURL(dialect: Dialect, wordId: string) {
+  let baseURL: string;
+  switch (dialect) {
+    case "forest":
+      baseURL = AudioResources.URL.forest;
+    case "reef":
+      baseURL = AudioResources.URL.reef;
+    default:
+      baseURL = AudioResources.URL.forest;
+  }
+  return `${baseURL}/${wordId}.mp3`
+}
+
 export function useSound(wordId: string) {
-  const player = useAudioPlayer(`${AudioResources.URL}/${wordId}.mp3`);
+  const { dialect } = useDialectContext();
+  const player = useAudioPlayer(getURL(dialect,wordId));
   const [disabled, setDisabled] = useState(false);
 
   const playSound = (): void => {
